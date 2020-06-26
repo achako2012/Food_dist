3778dfd8200037ba4505d99c825df043f24a943e
achako2012 <a.chako2012@gmail.com>
2020-06-26 21:12:31 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -157,7 +157,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
             render() {
                   const element = document.createElement('div');
-                  if(this.classes.length === 0 ){
+                  if (this.classes.length === 0) {
                         this.element = 'menu__item';
                         element.classList.add(this.element);
                   }
@@ -204,5 +204,47 @@ window.addEventListener('DOMContentLoaded', () => {
             21,
             ".menu .container",
       ).render();
+
+      //Forms
+
+      const forms = document.querySelectorAll('form');
+
+      const message = {
+            loading: 'загрузка',
+            success: 'успех',
+            failure: 'что то пошло не так...'
+      };
+
+      forms.forEach(item => {
+            postData(item);
+      });
+
+      function postData(form) {
+            form.addEventListener('submit', (e) => {
+                  e.preventDefault();
+
+                  const statusMessage = document.createElement('div');
+                  statusMessage.classList.add('status');
+                  statusMessage.textContent=message.loading;
+                  form.append(statusMessage);
+
+                  const request = new XMLHttpRequest();
+                  request.open('POST', 'server.php');
+
+                  request.setRequestHeader('Content-type', 'multipart/form-data');
+                  const formData = new FormData(form);
+
+                  request.send(formData);
+
+                  request.addEventListener('load', () => {
+                        if (request.status === 200) {
+                              console.log(request.response);
+                              statusMessage.textContent=message.success;
+                        }else{
+                              statusMessage.textContent=message.failure;
+                        }
+                  });
+            });
+      }
 
 });
